5529bbaf0e9e8a267c827af98a1904892a1feaff
hassan220022 <hassansonson2002@gmail.com>
2025-04-02 15:12:16 +0200

    Add 0x0B SSH project files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Puppet manifest to configure SSH client
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^#?\s*PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^#?\s*IdentityFile',
+} 
